3ca12dd4d495ec8d4fd935a52ed6d2f7aef3d79f
soulrpg <soulrpgmaker@gmail.com>
2023-09-29 01:22:52 +0200

    style: add missing strict_types and newline

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,12 +3,13 @@
 declare(strict_types=1);
 
 namespace Soulrpg\CgrdNewsApp\Controller;
+
 use Soulrpg\CgrdNewsApp\Repository\NewsRepository;
 use Soulrpg\CgrdNewsApp\Utility\FlashMessageUtility;
 
 class HomeController
 {
-    public function home(): void 
+    public function home(): void
     {
         $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../Templates');
         $twig = new \Twig\Environment($loader);
